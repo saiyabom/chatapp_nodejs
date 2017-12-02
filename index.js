3ba54a5f4f641bacce8ba9886513207fa402aea7
Siriphong Rattananoraset <Siriphong@siriphongs-mbp.ibsglite.com>
2017-12-02 18:41:20 +0700

    02_Implement the session store feature

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ import http from 'http'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 
+//Session store feature
+import session from 'express-session'
+import ConnectMongo from 'connect-mongo'
+const MongoStore = ConnectMongo(session);
+
+
 const app = express()
 
 import router from './router'
@@ -20,6 +26,18 @@ mongoose.connect('mongodb://root:1qaz2wsx@ds129066.mlab.com:29066/chatbox');
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({type:"*/*"}));
+
+//Session
+app.use(session({
+    secret:'1qaz2wsx',
+    saveUninitialized:true,
+    resave:true,
+    store: new MongoStore({
+        mongooseConnection:mongoose.connection,
+        ttl: 2 * 24 * 60 * 60,
+    })
+}))
+
 router(app);
 
 // Server Setup
